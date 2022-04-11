ef3b1f00251ea9c9ed2cc9947708873484326b77
Thanh Nam Tran <ttran19@umbc.edu>
2022-04-10 22:18:05 -0400

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import heroBcg from '../assets/hero-bcg.jpg'
+
+const Hero = () => {
+  return (
+    <Wrapper className="section-center">
+      <article className="content">
+        <h1>
+          Hãy thiết kế <br />
+          ngôi nhà của bạn
+        </h1>
+        <p>
+          Thanh Thanh Phương là một trong số nhà phân phối số 1 khu vực miền Nam chúng tôi chuyên phân
+          phối và cung cấp vật liệu xây dựng, trang trí nội và ngoại thất.
+        </p>
+        <Link to="/products" className="btn hero-btn">
+          xem thêm
+        </Link>
+      </article>
+      <article className="img-container">
+        <img src={heroBcg} alt="nice table" className="main-img" />
+      </article>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  min-height: 60vh;
+  display: grid;
+  place-items: center;
+  .img-container {
+    display: none;
+  }
+
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin-bottom: 2rem;
+    color: var(--clr-grey-5);
+    font-size: 1rem;
+  }
+  @media (min-width: 992px) {
+    height: calc(100vh - 5rem);
+    grid-template-columns: 1fr 1fr;
+    gap: 8rem;
+    h1 {
+      margin-bottom: 2rem;
+    }
+    p {
+      font-size: 1.25rem;
+    }
+    .hero-btn {
+      padding: 0.75rem 1.5rem;
+      font-size: 1rem;
+    }
+    .img-container {
+      display: block;
+      position: relative;
+    }
+    .main-img {
+      width: 100%;
+      height: 550px;
+      position: relative;
+      border-radius: var(--radius);
+      display: block;
+      object-fit: cover;
+    }
+    .accent-img {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 250px;
+      transform: translateX(-50%);
+      border-radius: var(--radius);
+    }
+    .img-container::before {
+      content: '';
+      position: absolute;
+      width: 10%;
+      height: 90%;
+      background: var(--clr-primary-9);
+      bottom: 0%;
+      left: -8%;
+      border-radius: var(--radius);
+    }
+  }
+`
+
+export default Hero

ed459b9f3327afbdb149363803a75bfe566d41f0
Thanh Nam Tran <ttran19@umbc.edu>
2022-04-22 01:49:53 -0400

    fix bugs

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -1,55 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
-import { useCartContext } from '../context/cart_context'
-import { useUserContext } from '../context/user_context'
-import { formatPrice } from '../utils/helpers'
-import { useHistory } from 'react-router-dom'
-import CartItem from './CartItem'
+// import React, { useState, useEffect } from 'react'
+// import styled from 'styled-components'
+// import { useCartContext } from '../context/cart_context'
+// import { useUserContext } from '../context/user_context'
+// import { formatPrice } from '../utils/helpers'
+// import { useHistory } from 'react-router-dom'
+// import CartItem from './CartItem'
 
-const CheckoutForm = () => {
-  const { cart, total_amount, shipping_fee, clearCart } = useCartContext();
-  const { myUser } = useUserContext();
-  const history = useHistory();
+// const CheckoutForm = () => {
+//   const { cart, total_amount, shipping_fee, clearCart } = useCartContext();
+//   const { myUser } = useUserContext();
+//   const history = useHistory();
 
-  return (
-    <div>
-      <h4>Xin chào, {myUser && myUser.name}</h4>
-      <p>Tổng thanh toán: {formatPrice(shipping_fee + total_amount)}</p>
-      <div className="content">
-        <h5>Sản phẩm</h5>
-        <h5>Đơn Giá</h5>
-        <h5>Số lượng</h5>
-        <h5>Số tiền</h5>
-        <span></span>
-      </div>
-      <hr />
-      {cart.map((item) => {
-        return <CartItem key={item.id} checkout={true} {...item} />;
-      })}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h4>Xin chào, {myUser && myUser.name}</h4>
+//       <p>Tổng thanh toán: {formatPrice(shipping_fee + total_amount)}</p>
+//       <div className="content">
+//         <h5>Sản phẩm</h5>
+//         <h5>Đơn Giá</h5>
+//         <h5>Số lượng</h5>
+//         <h5>Số tiền</h5>
+//         <span></span>
+//       </div>
+//       <hr />
+//       {cart.map((item) => {
+//         return <CartItem key={item.id} checkout={true} {...item} />;
+//       })}
+//     </div>
+//   );
+// }
 
-const StripeCheckout = () => {
-  return (
-    <Wrapper className="section section-center">
-      <CheckoutForm />
-    </Wrapper>
-  );
-}
+// const StripeCheckout = () => {
+//   return (
+//     <Wrapper className="section section-center">
+//       <CheckoutForm />
+//     </Wrapper>
+//   );
+// }
 
-const Wrapper = styled.section`
- @media (min-width: 776px) {
-    .content {
-      display: grid;
-      grid-template-columns: 275px 150px 390px 150px;
-      justify-items: center;
-      column-gap: 2rem;
-      h5 {
-        color: var(--clr-grey-5);
-        font-weight: 400;
-      }
-    }
-`;
+// const Wrapper = styled.section`
+//  @media (min-width: 776px) {
+//     .content {
+//       display: grid;
+//       grid-template-columns: 275px 150px 390px 150px;
+//       justify-items: center;
+//       column-gap: 2rem;
+//       h5 {
+//         color: var(--clr-grey-5);
+//         font-weight: 400;
+//       }
+//     }
+// `;
 
-export default StripeCheckout
+// export default StripeCheckout
--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,37 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero, StripeCheckout } from '../components'
+// import React from 'react'
+// import styled from 'styled-components'
+// import { PageHero, StripeCheckout } from '../components'
 
-// extra imports
-import { useCartContext } from '../context/cart_context'
-import { Link } from 'react-router-dom'
+// // extra imports
+// import { useCartContext } from '../context/cart_context'
+// import { Link } from 'react-router-dom'
 
-const CheckoutPage = () => {
-  const { cart } = useCartContext()
-  return (
-    <main>
-      <PageHero title="Thanh toán" />
-      <Wrapper className="page">
-        {cart.length < 1 ? (
-          <div className="empty">
-            <h2>Giỏ hàng của bạn đang trống</h2>
-            <Link to="/products" className="btn">
-              Bắt đầu tại đây
-            </Link>
-          </div>
-        ) : (
-          <StripeCheckout />
-        )}
-      </Wrapper>
-    </main>
-  );
-}
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  .empty {
-    text-align: center;
-  }
-`
-export default CheckoutPage
+// const CheckoutPage = () => {
+//   const { cart } = useCartContext()
+//   return (
+//     <main>
+//       <PageHero title="Thanh toán" />
+//       <Wrapper className="page">
+//         {cart.length < 1 ? (
+//           <div className="empty">
+//             <h2>Giỏ hàng của bạn đang trống</h2>
+//             <Link to="/products" className="btn">
+//               Bắt đầu tại đây
+//             </Link>
+//           </div>
+//         ) : (
+//           <StripeCheckout />
+//         )}
+//       </Wrapper>
+//     </main>
+//   );
+// }
+// const Wrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   .empty {
+//     text-align: center;
+//   }
+// `
+// export default CheckoutPage

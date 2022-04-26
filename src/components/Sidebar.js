ade230bf0515540fc2bc10d12f304a2970261aee
Thanh Nam Tran <ttran19@umbc.edu>
2022-04-26 01:25:53 -0400

    redeploy

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "../assets/Logo.png"
+import logo from "../assets/logo.png"
 import { Link } from 'react-router-dom'
 import { useProductsContext } from '../context/products_context'
 import { FaTimes } from 'react-icons/fa'
